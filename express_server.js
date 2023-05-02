b04f2e86b18881a7a6157569f591feff6b3453e2
Sethu Nair <sethunair2102@gmail.com>
2023-05-01 22:46:02 -0600

    Stores shortURL-LongURL key value pain in database and redirects to the longurl from shorturl

--- express_server.js
+++ express_server.js
@@ -11,35 +11,52 @@ const urlDatabase = {
 
 app.use(express.urlencoded({ extended: true }));
 
-function generateRandomString(length) {
+//function to create the short url id
+function generateRandomString() {
   let result = "";
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let charLength = chars.length;
-  for (let i = 0; i < length; i++){
+  for (let i = 0; i < 6; i++){
     result += chars.charAt(Math.floor(Math.random() * charLength));
   }
+  return result
 };
 
 app.get("/", (req, res) => {
   res.send('Hello');
 });
 
+//route that posts the new short url and the long url
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
-  //id = generateRandomString(6)
-})
+  let newID = generateRandomString();
+  urlDatabase[newID] = req.body.longURL;
+  console.log(urlDatabase);
+  // urlDatabase[newID] = {
+  //   shortURL: newID,
+  //   longURL: ,
+  // }
+  res.redirect(`/urls/${newID}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  console.log("a", urlDatabase);
+  console.log("b", urlDatabase[req.params.id]);
+  console.log("c", urlDatabase[req.params.id].longURL);
+  res.redirect(longURL);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]}
+   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render("urls_show", templateVars);
 });
 
