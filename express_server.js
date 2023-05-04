2e4bce581ab24da869b2ee1a926b605024119258
Sethu Nair <sethunair2102@gmail.com>
2023-05-03 22:02:34 -0600

    Added comments for routes

--- express_server.js
+++ express_server.js
@@ -23,7 +23,7 @@ function generateRandomString() {
 };
 
 app.get("/", (req, res) => {
-  res.send('Hello');
+  res.redirect("/urls")
 });
 
 //route that posts the new short url and the long url
@@ -34,31 +34,33 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newID}`);
 });
 
+//post route for the deletion of a url from the database
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls")
-})
+});
+
+//post route to update the longurl when client edits
 app.post("/urls/:id", (req,res) => {
   urlDatabase[req.params.id] = req.body.newUrl
-  // console.log(urlDatabase[req.params.id])
   res.redirect("/urls")
 })
 
-
+//redirects the short url to the long url browser
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
-
+// get route to render the index ejs on the urls page
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
-
+//renders the _new ejs when clients wants to create a new server
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
+// 
 app.get("/urls/:id", (req, res) => {
    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render("urls_show", templateVars);
