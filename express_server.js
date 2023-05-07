94ef236e7aec16653889513b1d5973e0c6a548f7
Sethu Nair <sethunair2102@gmail.com>
2023-05-07 13:40:50 -0600

    NEw user registration added

--- express_server.js
+++ express_server.js
@@ -13,20 +13,27 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+let users = {
+
+};
+
+let username;
+
 //function to create the short url id
 function generateRandomString() {
   let result = "";
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let charLength = chars.length;
-  for (let i = 0; i < 6; i++){
+  for (let i = 0; i < 6; i++) {
     result += chars.charAt(Math.floor(Math.random() * charLength));
   }
   return result
 };
 
-let username = false;
+
 
 app.get("/", (req, res) => {
+  // username = req.cookies.user_ID;
   res.redirect("/urls");
 });
 
@@ -34,17 +41,31 @@ app.get("/", (req, res) => {
 app.post("/login", (req, res) => {
   username = req.body.username;
   res.cookie("username", username);
-  res.redirect("/urls"); 
+  res.redirect("/urls");
 });
 
+//Posts the registration information
+app.post("/register", (req, res) => {
+  userID = generateRandomString();
+  users[userID] = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password
+  };
+  res.cookie("user_ID", userID);
+
+  res.redirect("/urls");
+})
+
+// Route to get the register page
 app.get("/register", (req, res) => {
-  const templateVars = {username: username};
+  const templateVars = { user: users[username] };
   res.render("urls_registration", templateVars);
 })
 
 //logout route
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_ID");
   res.redirect("/");
 })
 
@@ -63,7 +84,7 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 //post route to update the longurl when client edits
-app.post("/urls/:id", (req,res) => {
+app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.newUrl
   res.redirect("/urls")
 })
@@ -75,35 +96,21 @@ app.get("/u/:id", (req, res) => {
 });
 // get route to render the index ejs on the urls page
 app.get("/urls", (req, res) => {
-  username = req.cookies.username;
-  let templateVars;
-  if(username) { 
-    templateVars = { urls: urlDatabase, username: username};
-  } else {
-    templateVars = { urls: urlDatabase, username: false}
-  }
-  res.render("urls_index", templateVars); 
+  username = req.cookies.user_ID;
+  // console.log(users[username], "this is the username"); 
+  templateVars = { urls: urlDatabase, user: users[username] };
+  res.render("urls_index", templateVars);
 });
 //renders the _new ejs when clients wants to create a new server
 app.get("/urls/new", (req, res) => {
-  username = req.cookies.username;
-  let templateVars;
-  if(username) { 
-    templateVars = { username: username};
-  } else {
-    templateVars = { username: false}
-  }
+  username = req.cookies.user_ID;
+  templateVars = { user: users[username] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  username = req.cookies.username;
-  let templateVars;
-  if(username) { 
-    templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], urls: urlDatabase, username: username};
-  } else {
-    templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], urls: urlDatabase, username: false}
-  }
+  username = req.cookies.user_ID;
+  templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], urls: urlDatabase, user: users[username] };
   res.render("urls_show", templateVars);
 });
 
